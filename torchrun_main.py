f219acf50ae2d429fbe1cb071ff13f75b0634997
modernTalker <116522860+modernTalker@users.noreply.github.com>
2025-07-18 13:22:31 +0300

    Update torchrun_main.py: with torch._dynamo.disable():

--- torchrun_main.py
+++ torchrun_main.py
@@ -284,57 +284,58 @@ def save_checkpoint(args, model, optimizer, scheduler, global_rank, world_size, 
 @torch.no_grad()
 def evaluate_model(model, preprocess_batched, pad_idx, global_rank, world_size, device, batch_size, 
                    args_dtype, args_amp, short_debug_eval=False):
-    _time = time.time()
+    with torch._dynamo.disable():
+        _time = time.time()
+        
+        if "INPUT_PATH" in os.environ or "c4_val" in os.listdir(os.getcwd()):
+            input_path = os.environ["INPUT_PATH"] if "INPUT_PATH" in os.environ else os.getcwd()
+            data_files = {"val": [os.path.join(input_path, "c4_val/*.json.gz")]}
+            val_data = datasets.load_dataset('json', data_files=data_files, split='val', streaming=args.streaming)
+        else:
+            val_data = datasets.load_dataset("allenai/c4", "en", split="validation", streaming=True)
     
-    if "INPUT_PATH" in os.environ or "c4_val" in os.listdir(os.getcwd()):
-        input_path = os.environ["INPUT_PATH"] if "INPUT_PATH" in os.environ else os.getcwd()
-        data_files = {"val": [os.path.join(input_path, "c4_val/*.json.gz")]}
-        val_data = datasets.load_dataset('json', data_files=data_files, split='val', streaming=args.streaming)
-    else:
-        val_data = datasets.load_dataset("allenai/c4", "en", split="validation", streaming=True)
-
-    val_data = val_data.shuffle(seed=42)
-    logger.info(f"Loaded validation dataset in {time.time() - _time:.2f} seconds")
-
-    if not args.single_gpu:
-        val_data = datasets.distributed.split_dataset_by_node(val_data, rank=global_rank, world_size=world_size)
-
-    val_data_mapped = val_data.map(
-        preprocess_batched,
-        batched=True,
-        remove_columns=["text", "timestamp", "url"],
-    )
-    val_data_mapped.batch = lambda batch_size: training_utils.batch_fn(val_data_mapped, batch_size)
-
-    target_eval_tokens = 10_000_000
-    evaluated_on_tokens = 0
-    total_loss = torch.tensor(0.0).to(device)
-    total_batches = 0
-    logger.info(f"Eval set prepared in {time.time() - _time:.2f} seconds")
-
-    max_batches = int(200 / int(os.environ["WORLD_SIZE"]) * (batch_size / 256)) + 1 if not short_debug_eval else 3
-    for batch in val_data_mapped.batch(batch_size=batch_size):
-        if total_batches == max_batches:
-            break
-
-        total_batches += 1
-        batch = {k: v.to(device) for k, v in batch.items()}
-        labels = batch["input_ids"].clone()
-        labels[labels == pad_idx] = -100
-        with autocast(device_type='cuda', dtype=torch.bfloat16 if not args_dtype == torch.float16 else torch.float16, enabled=args_amp):
-            loss = model(**batch, labels=labels).loss
-        total_loss += loss.detach()
-
-        evaluated_on_tokens += (batch["input_ids"] != pad_idx).sum().item() * world_size
-
-    total_loss = total_loss / total_batches
-
-    # Gather losses across all GPUs
-    gathered_losses = [torch.zeros_like(total_loss) for _ in range(world_size)]
-    dist.all_gather(gathered_losses, total_loss)
-    total_loss = sum([t.item() for t in gathered_losses]) / world_size
-
-    stable_rank_dict = None
+        val_data = val_data.shuffle(seed=42)
+        logger.info(f"Loaded validation dataset in {time.time() - _time:.2f} seconds")
+    
+        if not args.single_gpu:
+            val_data = datasets.distributed.split_dataset_by_node(val_data, rank=global_rank, world_size=world_size)
+    
+        val_data_mapped = val_data.map(
+            preprocess_batched,
+            batched=True,
+            remove_columns=["text", "timestamp", "url"],
+        )
+        val_data_mapped.batch = lambda batch_size: training_utils.batch_fn(val_data_mapped, batch_size)
+    
+        target_eval_tokens = 10_000_000
+        evaluated_on_tokens = 0
+        total_loss = torch.tensor(0.0).to(device)
+        total_batches = 0
+        logger.info(f"Eval set prepared in {time.time() - _time:.2f} seconds")
+    
+        max_batches = int(200 / int(os.environ["WORLD_SIZE"]) * (batch_size / 256)) + 1 if not short_debug_eval else 3
+        for batch in val_data_mapped.batch(batch_size=batch_size):
+            if total_batches == max_batches:
+                break
+    
+            total_batches += 1
+            batch = {k: v.to(device) for k, v in batch.items()}
+            labels = batch["input_ids"].clone()
+            labels[labels == pad_idx] = -100
+            with autocast(device_type='cuda', dtype=torch.bfloat16 if not args_dtype == torch.float16 else torch.float16, enabled=args_amp):
+                loss = model(**batch, labels=labels).loss
+            total_loss += loss.detach()
+    
+            evaluated_on_tokens += (batch["input_ids"] != pad_idx).sum().item() * world_size
+    
+        total_loss = total_loss / total_batches
+    
+        # Gather losses across all GPUs
+        gathered_losses = [torch.zeros_like(total_loss) for _ in range(world_size)]
+        dist.all_gather(gathered_losses, total_loss)
+        total_loss = sum([t.item() for t in gathered_losses]) / world_size
+    
+        stable_rank_dict = None
 
     return total_loss, evaluated_on_tokens, stable_rank_dict
 
